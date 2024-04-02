015caf502fc1f32b77d276d418551b589caa91fc
alexlasota <alexylasota@gmail.com>
2024-04-02 20:18:28 +0200

    ProjectTest 1

--- src/com/shop/manager/ProductProcessor.java
+++ src/com/shop/manager/ProductProcessor.java
@@ -13,25 +13,36 @@ public class ProductProcessor implements ProductManagement {
         this.products = new ArrayList<>();
     }
 
+    /**
+     * Adds a new product to the processor.
+     * @param product The product to be added.
+     */
     public void addProduct(Product product) {
         products.add(product);
     }
 
+    /**
+     * Removes a product from the processor.
+     * @param product The product to be removed.
+     */
     public void removeProduct(Product product) {
         products.remove(product);
     }
 
+    /**
+     * Returns a list of all products in the processor.
+     * @return A list containing all products.
+     */
     public List<Product> getProducts() {
         return products;
     }
 
-    /**
-     *
-     * @param id
-     * @return
-     */
 
-    //easada
+    /**
+     * Finds a product with the given identifier.
+     * @param id The identifier of the product to be found.
+     * @return The product with the given identifier, or null if the product is not found.
+     */
     public Product getProductById(long id) {
         for (Product product : products) {
             if (product.getId() == id) {
@@ -41,6 +52,10 @@ public class ProductProcessor implements ProductManagement {
         return null;
     }
 
+    /**
+     * Updates an existing product in the processor.
+     * @param updatedProduct The updated product to replace the existing product.
+     */
     public void updateProduct(Product updatedProduct) {
         for (int i = 0; i < products.size(); i++) {
             if (products.get(i).getId() == updatedProduct.getId()) {
